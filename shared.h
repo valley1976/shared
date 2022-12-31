5dcf5b854cfc5f827ac53db9a8f19f00c3304f55
valley1976 <121720871+valley1976@users.noreply.github.com>
2022-12-31 22:26:16 +0800

    Update shared.h
    add MIT LICENSE

--- shared.h
+++ shared.h
@@ -1,5 +1,9 @@
 /*
  *  Copyright (c) 2022-2023 shangu. All Right Reserved
+ 
+ *  This is free software; you can redistribute it and/or modify it under the
+ *  terms of the MIT license. A copy of the license can be found in the file
+ *  "LICENSE" at the root of this distribution.
  *
  *  author:  shangu
  *  Date:    2022-12-31
